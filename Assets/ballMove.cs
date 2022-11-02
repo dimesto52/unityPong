9418c650457429ac9c519a7f2f953d264daddec0
dimesto52 <98359317+dimesto52@users.noreply.github.com>
2022-11-02 14:32:13 +0100

    ep02 la balle

---
+++ Assets/ballMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMove : MonoBehaviour
+{
+
+    Vector2 direction = Vector2.zero;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+
+        direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)direction * Time.deltaTime * speed;  
+    }
+}
