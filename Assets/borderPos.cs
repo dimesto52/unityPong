0a6ba08b2df41b1da3858103a24d3a5fa12797f9
dimesto52 <98359317+dimesto52@users.noreply.github.com>
2022-11-22 16:17:09 +0100

    ep 07 better Pad

--- Assets/borderPos.cs
+++ Assets/borderPos.cs
@@ -7,12 +7,14 @@ public class borderPos : MonoBehaviour
 {
     public borderpos pos = borderpos.top;
 
-    // Start is called before the first frame update
-    void Start()
+    private float curAspect = 0.0f;
+
+    void refreshPos()
     {
         Vector3 camPos = Camera.main.transform.position;
         float camH = Camera.main.orthographicSize;
         float camW = Camera.main.orthographicSize * Camera.main.aspect;
+        curAspect = Camera.main.aspect;
 
         transform.position = camPos;
 
@@ -22,8 +24,8 @@ public class borderPos : MonoBehaviour
         switch (pos)
         {
             case borderpos.top:
-                transform.position += Vector3.up * (camH+0.5f);
-                box.size = new Vector2(2 * camW,1.0f);
+                transform.position += Vector3.up * (camH + 0.5f);
+                box.size = new Vector2(2 * camW, 1.0f);
                 break;
 
             case borderpos.bottom:
@@ -32,7 +34,7 @@ public class borderPos : MonoBehaviour
                 break;
 
             case borderpos.right:
-                transform.position += Vector3.right * (camW+ 0.5f);
+                transform.position += Vector3.right * (camW + 0.5f);
                 box.size = new Vector2(1.0f, 2 * camH);
                 break;
 
@@ -41,13 +43,25 @@ public class borderPos : MonoBehaviour
                 box.size = new Vector2(1.0f, 2 * camH);
                 break;
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        refreshPos();
+
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(curAspect != Camera.main.aspect)
+        {
+            refreshPos();
+        }
+
     }
 }
 public enum borderpos
--- Assets/padMove.cs
+++ Assets/padMove.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class padMove : MonoBehaviour
 {
 
     public playerSide side = playerSide.Left;
 
     public float speed = 5.0f;
+    public float distance = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = Camera.main.transform.position;
+        Vector3 d = Vector3.zero;
+        if (side == playerSide.Left) d = Vector3.left;
+        else d = Vector3.right;
+
+        transform.position = d * Camera.main.aspect * Camera.main.orthographicSize * 0.9f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = Camera.main.transform.position;
+        Vector3 d = Vector3.zero;
+        if (side == playerSide.Left) d = Vector3.left;
+        else d = Vector3.right;
+
+        transform.position = new Vector3(0, transform.position.y) + d * Camera.main.aspect * Camera.main.orthographicSize * 0.9f;
 
         if (
             (side == playerSide.Left && Input.GetKey(KeyCode.A)) ||
